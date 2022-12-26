ccb770d7a734629370cbb5ed8ad09c8931f90c6d
Sławomir Kowal <vanter2x@gmail.com>
2022-12-26 19:23:36 +0100

    Clear Project and add swagger

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Work Time Logger API",
+        Description = "An ASP.NET Core Web API for managing work time",
+        Contact = new OpenApiContact
+        {
+            Name = "S³awek Kowal",
+            Email = "vanter2x@gmail.com"
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -13,10 +28,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
