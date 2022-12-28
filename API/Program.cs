cafaee06840ba0dea13b8cf415aedff4edb7ee46
Sławomir Kowal <vanter2x@gmail.com>
2022-12-28 21:41:53 +0100

    add navbar and cors

--- API/Program.cs
+++ API/Program.cs
@@ -5,60 +5,60 @@ using Microsoft.OpenApi.Models;
 using Persistence;
 using Persistence.DataSeeding;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder (args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers ();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
+builder.Services.AddEndpointsApiExplorer ();
+builder.Services.AddSwaggerGen (options => {
+    options.SwaggerDoc ("v1", new OpenApiInfo {
         Version = "v1",
-        Title = "Work Time Logger API",
-        Description = "An ASP.NET Core Web API for managing work time",
-        Contact = new OpenApiContact
-        {
-            Name = "S³awek Kowal",
-            Email = "vanter2x@gmail.com"
-        }
+            Title = "Work Time Logger API",
+            Description = "An ASP.NET Core Web API for managing work time",
+            Contact = new OpenApiContact {
+                Name = "Sï¿½awek Kowal",
+                    Email = "vanter2x@gmail.com"
+            }
     });
 });
 
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.Services.AddDbContext<DataContext> (opt => {
+    opt.UseSqlServer (builder.Configuration.GetConnectionString ("DefaultConnection"));
 });
 
-builder.Services.AddScoped<IDataGenerator, DataGenerator>();
-builder.Services.AddScoped<Seeder>();
+builder.Services.AddScoped<IDataGenerator, DataGenerator> ();
+builder.Services.AddScoped<Seeder> ();
 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddMediatR(typeof(UserList.Handler));
+builder.Services.AddAutoMapper (AppDomain.CurrentDomain.GetAssemblies ());
+builder.Services.AddMediatR (typeof (UserList.Handler));
 
+builder.Services.AddCors (opt => {
+    opt.AddPolicy ("CorsPolicy", policy => {
+        policy.AllowAnyMethod ().AllowAnyHeader ().WithOrigins ("http://localhost:3001");
+    });
+});
 
+var app = builder.Build ();
 
-var app = builder.Build();
+app.UseCors ("CorsPolicy");
 
 // Configure the HTTP request pipeline.
-var scope = app.Services.CreateScope();
-var seeder = scope.ServiceProvider.GetRequiredService<Seeder>();
+var scope = app.Services.CreateScope ();
+var seeder = scope.ServiceProvider.GetRequiredService<Seeder> ();
 
-seeder.SeedData();
+seeder.SeedData ();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+if (app.Environment.IsDevelopment ()) {
+    app.UseSwagger ();
+    app.UseSwaggerUI (options => {
+        options.SwaggerEndpoint ("/swagger/v1/swagger.json", "v1");
     });
 }
 
-app.UseAuthorization();
+app.UseAuthorization ();
 
-app.MapControllers();
+app.MapControllers ();
 
-app.Run();
+app.Run ();
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import ButtonAppBar from './Components/NavBar';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:7156/api/user')
+    .then(response => {
+      setUsers(response.data);
+    })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
+      <ButtonAppBar/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ul>
+        {users.map((user : any) => (
+            <li key={user.id}>
+              {user.firstName}
+            </li>
+        ))}
+        </ul>
     </div>
   );
 }
